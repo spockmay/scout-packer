5b137191dbdf3984fa6ee8a2353d51077edf7a78
Ryan May <spockmay@users.noreply.github.com>
2026-05-04 10:41:14 -0400

    force reload of the worker, version bump

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scout-packer-v1.0.1';
+const CACHE_NAME = 'scout-packer-v1.0.2';
 const ASSETS = [
   './',
   './index.html',
@@ -15,6 +15,7 @@ const ASSETS = [
 
 // 1. Install - Save the "Survival Kit"
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forces the waiting service worker to become active
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
