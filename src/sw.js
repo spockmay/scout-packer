f4f2ffacea3c3fcfd991fadf871cd4e3d10f7474
Ryan May <spockmay@users.noreply.github.com>
2026-05-07 15:36:36 -0400

    keep trying to get this cache situation taken care of

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scout-packer-v1.0.2';
+const CACHE_NAME = 'scout-packer-v1.0.3';
 const ASSETS = [
   './',
   './index.html',
@@ -19,6 +19,22 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+});
+
+// delete the old cache when a new one is found
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Clearing old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
 
 // 2. Fetch - The Proxy Logic
